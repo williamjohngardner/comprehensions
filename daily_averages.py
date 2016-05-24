ae62dca99ccfde81b3a5fdfa407b9d6641375d5a
williamjohngardner <bill@williamjohngardner.com>
2016-05-24 17:28:33 -0400

    5:30

--- daily_averages.py
+++ daily_averages.py
@@ -2,20 +2,35 @@
 
 import datetime
 from wave_height import wave_height
-from day_conversion import get_day_of_week
+from collections import defaultdict
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 dictionary = wave_height()
 
-dates = [value for value in dictionary]
+# print(dictionary)
+
+nested_dictionary = {}
+
+for key, value in dictionary.items():
+    day_index = datetime.datetime.strptime(key, "%Y-%m-%d").weekday()
+    nested_dictionary = [days[day_index], value]
+    # print(nested_dictionary)
+
+avg_dictionary = defaultdict()
+
+for key, value in nested_dictionary:
+    avg_dictionary[key].append(value)
+    print(avg_dictionary)
+
+
+
+# dates = {value for value in dictionary.items()}
 # for each value in a dictionary, return the 'value' at the iterable index.
-dates_string = ' '.join(dates)
+# print(dates)
+# dates_string = ' '.join(dates)
 # print(dates_string)
 
-days = []
-def get_dow(date_string):
-    return days[datetime.datetime.strptime(dates_string, "%Y-%m-%d").weekday()]
-
-print([get_dow(dates_string) for day in days])
 
 
 # return average
