60edcbecbb32c1b4baa2593f360a8a9e5a93f9a6
williamjohngardner <bill@williamjohngardner.com>
2016-05-23 20:08:40 -0400

    8:00 commit

--- wave_height.py
+++ wave_height.py
@@ -4,17 +4,10 @@ def wave_height():
 
     temperatures = csv_parse()
 
-    input_set = []
+    dates = [item[5] for item in temperatures]
+    heights = [item[1] for item in temperatures]
 
-    for item in temperatures:
-        input_set = [item[5], item[1]]
+    dictionary = dict(zip(dates, heights))
+    print(dictionary)
 
-    headers = ["Date", "Wave Height"]
-
-    wave_height = {}
-    wave_height["Date"] = input_set[0]
-    wave_height["Wave Height"] = input_set[1]
-    wave_height = {header: input_set[index] for index, header in enumerate(headers)}
-
-    wave_height_list = [list(wave_height.values()) for items in wave_height]
-    return wave_height_list
+    return dictionary
